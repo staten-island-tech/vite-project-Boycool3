7bf6211eb476b341b21d49b80a030f4223cdc9df
Boycool3 <christian.beaz91@gmail.com>
2025-11-25 12:26:28 -0500

    yay js works now

--- vite-project/index.js
+++ vite-project/index.js
@@ -1,6 +1,3 @@
-import './styles.css'
-
-
 document.querySelector(".btn").addEventListener("click", function () {
   if (document.body.classList.contains("light")) {
     document.body.classList.add("dark");

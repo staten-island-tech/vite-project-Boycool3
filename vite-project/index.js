49de4bcf9f96c81e4733bd6590f7d6dc13a50790
Boycool3 <christian.beaz91@gmail.com>
2025-12-01 11:51:37 -0500

    slight changes idk

--- vite-project/index.js
+++ vite-project/index.js
@@ -7,3 +7,15 @@ document.querySelector(".btn").addEventListener("click", function () {
     document.body.classList.remove("dark");
   }
 });
+/*function inject(item) {
+  const container = document.querySelector(".container");
+  container.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="card" data-title=${item.name}>
+      <h1 class="name">${item.name}</h1>
+      <img class="imag" src="${item.img}"/>
+      <h2 class="price">${item.price}$</h2>
+      <button class="add-btn">Add to Cart</button>
+    </div>`
+  );
+}*/

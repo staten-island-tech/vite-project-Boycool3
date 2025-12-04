23d9c52a9b2bf5a6a9ad212015818929b471df43
Boycool3 <christian.beaz91@gmail.com>
2025-12-04 12:31:55 -0500

    array of movies stuff

--- vite-project/index.js
+++ vite-project/index.js
@@ -7,15 +7,133 @@ document.querySelector(".btn").addEventListener("click", function () {
     document.body.classList.remove("dark");
   }
 });
-/*function inject(item) {
-  const container = document.querySelector(".container");
+const Movies = [
+  {
+    name: "The Greatest Showman",
+    img: "showsh man.jpg",
+    Genre1: "Musical",
+    Genre2: "Thriller",
+    watched: true,
+  },
+  {
+    name: "Spider-Man: Into the Spider-Verse",
+    img: " ",
+    Genre1: "Family",
+    Genre2: "Action",
+    watched: true,
+  },
+  {
+    name: "Spider-Man: Across the Spider-Verse",
+    img: " ",
+    Genre1: "Action",
+    Genre2: "Sci-Fi",
+    watched: true,
+  },
+  {
+    name: "Black Panther",
+    img: " ",
+    Genre1: "Action",
+    Genre2: "Sci-Fi",
+    watched: true,
+  },
+  {
+    name: "Nobody",
+    img: " ",
+    Genre1: "Action",
+    Genre2: "Thriller",
+    watched: true,
+  },
+  {
+    name: "El Camino: A Breaking Bad Movie",
+    img: " ",
+    Genre1: "Thriller",
+    Genre2: "Crime",
+    watched: false,
+  },
+  {
+    name: "Pulp Fiction",
+    img: " ",
+    Genre1: "Crime",
+    Genre2: "Thriller",
+    watched: false,
+  },
+  {
+    name: "The Great Gatsby",
+    img: " ",
+    Genre1: "Romance",
+    Genre2: "Drama",
+    watched: false,
+  },
+  {
+    name: "Bohemian Rhapsody",
+    img: " ",
+    Genre1: "Musical",
+    Genre2: "Drama",
+    watched: false,
+  },
+  {
+    name: "Piece By Piece",
+    img: " ",
+    Genre1: "Musical",
+    Genre2: "Documentary",
+    watched: false,
+  },
+  {
+    name: "The Lego Movie",
+    img: " ",
+    Genre1: "Family",
+    Genre2: "Comedy",
+    watched: true,
+  },
+  {
+    name: "The Lego Batman Movie",
+    img: " ",
+    Genre1: "Family",
+    Genre2: "Comedy",
+    watched: true,
+  },
+  {
+    name: "Hamilton",
+    img: " ",
+    Genre1: "Musical",
+    Genre2: "Drama",
+    watched: true,
+  },
+  {
+    name: "Puss in Boots: The Last Wish",
+    img: " ",
+    Genre1: "Adventure",
+    Genre2: "Comedy",
+    watched: false,
+  },
+  {
+    name: "The Wild Robot",
+    img: " ",
+    Genre1: "Family",
+    Genre2: "Sci-Fi",
+    watched: false,
+  },
+]
+Movies.forEach((Movie) => inject(Movie));
+function inject(item) {
+  if(item.watched === true){
+  const container = document.querySelector(".containerwatched");
   container.insertAdjacentHTML(
     "afterbegin",
     `<div class="card" data-title=${item.name}>
       <h1 class="name">${item.name}</h1>
       <img class="imag" src="${item.img}"/>
-      <h2 class="price">${item.price}$</h2>
-      <button class="add-btn">Add to Cart</button>
+      <h2 class="genre">${item.Genre1}/${item.Genre2}</h2>
     </div>`
-  );
-}*/
+  );}
+  if(item.watched === false){
+  const container = document.querySelector(".containernot");
+  container.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="card" data-title=${item.name}>
+      <h1 class="name">${item.name}</h1>
+      <img class="imag" src="${item.img}"/>
+      <h2 class="genre">${item.Genre1}/${item.Genre2}</h2>
+    </div>`
+  );}
+}
